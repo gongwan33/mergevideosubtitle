be35ddafeb3d85094f5afb89cdd6e57b311aff4e
Gong Wong <gongwan33@gmail.com>
2021-11-15 22:39:31 +1300

    first version

--- README.md
+++ README.md
@@ -1,4 +1,23 @@
 # Video cooker
 Merge video, subtitle and watermark using ffmpeg
 
-Run cooker.ps in powershell to merge your video, subtitle and wartermark.
+Run `cooker.ps1` in powershell to merge your video, subtitle and wartermark.
+
+## Important
+
+If you haven't enable script execution on your machine, please start powershell as administration and run the following command:
+
+```
+set-executionpolicy remotesigned
+```
+
+## Quick start
+
+- Put `ffmpeg.exe` into this folder
+- Run `cooker.ps1`
+- Choose watermark file
+- Choose video foler (currently only support *.mp4 files)
+- Choose subtitle foler
+- Type in extension for subtitle. For example: ass or csv.ass or other as needed.
+
+The name of subtitle must be <video_file_name>_ass.<extension>. For example, if the video file name is `sample1.mp4`, and the subtitle extension you typed in is `ass`, the subtitle file name should be set to `sample1_ass.ass`.
---
+++ cooker.ps1
@@ -0,0 +1,95 @@
+﻿ Function Select-FolderDialog
+{
+    param([string]$Description="Select folder",[string]$RootFolder="Desktop")
+
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+
+    $objForm = New-Object System.Windows.Forms.FolderBrowserDialog
+    $objForm.Rootfolder = $RootFolder
+    $objForm.Description = $Description
+    $Show = $objForm.ShowDialog((New-Object System.Windows.Forms.Form -Property @{TopMost = $true }))
+    If ($Show -eq "OK")
+    {
+        Return $objForm.SelectedPath
+    }
+    Else
+    {
+        Write-Error "Operation cancelled by user."
+    }
+}
+
+ Function Select-FileDialog
+{
+    param([string]$Description="Select file",[string]$RootFolder="Desktop")
+
+    [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
+
+    $objForm = New-Object System.Windows.Forms.OpenFileDialog
+    $objForm.InitialDirectory = $RootFolder
+    $objForm.Title = $Description
+    $Show = $objForm.ShowDialog((New-Object System.Windows.Forms.Form -Property @{TopMost = $true }))
+    If ($Show -eq "OK")
+    {
+        Return $objForm.FileName
+    }
+    Else
+    {
+        Write-Error "Operation cancelled by user."
+    }
+}
+
+$waterMarkPath = Select-FileDialog("Select your watermark")
+echo "Your watermark is: $waterMarkPath"
+
+if (!$waterMarkPath) {
+    exit
+}
+
+$videoPath = Select-FolderDialog("Select your video folder")
+echo "Your video folder is: $videoPath"
+
+if (!$videoPath) {
+    exit
+}
+
+$subtitlePath = Select-FolderDialog("Select your subtitle foler")
+echo "Your subtitle folder is: $subtitlePath"
+
+$subExt = Read-Host -Prompt "Please input your subtitle file extension (e.g. ass or csv.ass)"
+
+if (!$subtitlePath) {
+    exit
+}
+
+echo ""
+echo "==================Cooking================="
+
+Get-ChildItem $videoPath |
+Foreach-Object {
+    $videoFullName = $_.FullName
+    echo "Found video file: $videoFullName"
+
+    $fileName = [io.path]::GetFileNameWithoutExtension($_.FullName)
+    $subtitleFile = "$subtitlePath\${fileName}_ass.$subExt"
+    echo "Looking for matching ass file: $subtitleFile"
+
+    if (![System.IO.File]::Exists($subtitleFile)) {
+        echo "Error: $subtitleFile do not exist"
+        echo ""
+        return
+    }
+
+    echo "Subtitle found ..."
+    echo "Start processing ..."
+
+    $waterMarkWinPath = $waterMarkPath -replace ":", "\\:"
+    $waterMarkWinPath = $waterMarkWinPath -replace "\\", "\\\\\"
+
+    $subtitleFileWinPath = $subtitleFile -replace ":", "\\:"
+    $subtitleFileWinPath = $subtitleFileWinPath -replace "\\", "\\\\\"
+
+    .\ffmpeg.exe -y -i "$videoFullName" -c:v libx264 -vf “movie=${waterMarkWinPath}[wm];[i][wm]overlay=24:10,subtitles=${subtitleFileWinPath}” ${fileName}_ass.mp4
+
+    echo "Finished processing for $videoFullName"
+    echo ""
+}
